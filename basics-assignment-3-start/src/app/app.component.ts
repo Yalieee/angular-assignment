148861c20b851fa18cc2535851d52b740a6e7c10
Yalieee <zeldago2000@gmail.com>
2017-11-26 15:44:35 +0800

    Finish basic-assignment-3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  hideText = true;
+  clickLogs = [];
+
+  onClick() {
+    if (this.hideText) {
+      this.hideText = false;
+    }
+
+    this.clickLogs.push(Date.now());
+  }
+
+  getColor() {
+    if (this.clickLogs.length >= 5) {
+      return 'blue';
+    }
+
+    return 'white';
+  }
 }
